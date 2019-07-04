02ce4d0c6cb31d3083aa58f9702a728e92dd2925
k11463 <k11463@yahoo.com.tw>
2019-07-04 16:57:49 +0800

    php artisan make:request StoreBlogPost

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'k7766134@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'brian'),
     ],
 
     /*
